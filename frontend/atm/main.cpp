ab76f7c50fc41687ee42c9735fd5472d200ff88a
rronkkeli <67864350+rronkkeli@users.noreply.github.com>
2024-04-08 16:10:31 +0300

    Moved main executable to frontend/atm

--- frontend/atm/main.cpp
+++ frontend/atm/main.cpp
@@ -1,11 +1,11 @@
-#include "mainwindow.h"
+#include "mw1.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    mw1 w;
     w.show();
     return a.exec();
 }
