da5b6def0f3541ab32b2f617c725efa37758bed6
rronkkeli <67864350+rronkkeli@users.noreply.github.com>
2024-04-15 15:50:09 +0300

    Moved updated loginDLL

--- frontend/loginDLL/logindll.h
+++ frontend/loginDLL/logindll.h
@@ -16,18 +16,60 @@ class LOGINDLL_EXPORT LoginDLL : public QObject
 public:
     LoginDLL(QObject * parent);
     ~LoginDLL();
+
     void login(QString cardId, QString cardPin);
+
+    void getCardInformation();
+
+    void getAccountInformation();
+
+    void logout();
+
+    void setCardNum(const QString &newCardNum);
+
+    void setWebToken(const QByteArray &newWebToken);
+
+    void setUserId(const QString &newUserId);
+
+    void setAccountId(const QString &newAccountId);
+
+    void getCustomerInfo();
+
+    void getWithdrawalsInfo();
+
+    void getTilitjaKortitInfo();
+
+    void nostotapahtuma(QString tilin_numero,QString nostot);
 
 private:
     QNetworkAccessManager *loginManager;
+    QNetworkAccessManager *cardInfoManager;
+    QNetworkAccessManager *accountInfoManager;
+    QNetworkAccessManager *getManager;
+    QNetworkAccessManager *postManager;
     QNetworkReply *reply;
     QByteArray response_data;
 
+    QString cardNum;
+    QByteArray webToken;
+    QString userId;
+    QString accountId;
+
 private slots:
     void loginSlot(QNetworkReply *reply);
+    void cardInfoSlot(QNetworkReply *reply);
+    void accountInfoSlot(QNetworkReply *reply);
+    void getCustomerSlot (QNetworkReply *reply);
+    void getWithdrawalsSlot (QNetworkReply *reply);
+    void getTilitjaKortitSlot (QNetworkReply *reply);
+    void getNostotapahtumaSlot (QNetworkReply *reply);
 
 signals:
     void loginStatus(QString);
+    void customerInfo(QString);
+    void withdrawalsInfo(QString withdrawal);
+    void tilitjakortitInfo(QString tilit, QString kortit);
+    void nostotapahtumaInfo(QString);
 };
 
 #endif // LOGINDLL_H
