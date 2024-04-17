ab0dc844caf2639990452a8841a1a1a884a468ee
rronkkeli <67864350+rronkkeli@users.noreply.github.com>
2024-04-17 20:01:45 +0300

    Added test data to database

--- db/bank.sql
+++ db/bank.sql
@@ -140,3 +140,38 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Test value insertions on all tables
+-- -----------------------------------------------------
+INSERT INTO bank.customer VALUES (1, 'Aku', 'Ankka', '+302385022982', '1934-03-13', 'Paratiisitie 13, Ankkalinna');
+INSERT INTO bank.customer VALUES (2, 'Iines', 'Ankka', '+1234567890', '1920-10-20', 'Helytie 6, Ankkalinna');
+-- Pin 1234;
+INSERT INTO bank.card VALUES ('06000622F0', '$2a$12$eoRO4an4QdEsUhbhwct52uNwqPFKFsLCim2GWpmaly3z77.H8xKAi', 1);
+-- Pin 4321;
+INSERT INTO bank.card VALUES ('06000DE540', '$2a$12$QnIME1de3u7kdAcMW1VVW.fiUAErV/WPSkTBGyEtEM1kzAV/yIKYG', 2);
+INSERT INTO bank.account VALUES (1, 20.00, NULL);
+INSERT INTO bank.account VALUES (2, 1923.71, 2000.00);
+INSERT INTO bank.account VALUES (3, 480.00, 500.00);
+INSERT INTO bank.card_has_account VALUES ('06000622F0', 1);
+INSERT INTO bank.card_has_account VALUES ('06000DE540', 2);
+INSERT INTO bank.card_has_account VALUES ('06000622F0', 3);
+INSERT INTO bank.customer_has_account VALUES (1, 1);
+INSERT INTO bank.customer_has_account VALUES (2, 2);
+INSERT INTO bank.customer_has_account VALUES (1, 3);
+INSERT INTO bank.withdrawal VALUES (1, 2, 50.00, '2022-11-06 10:31:11');
+INSERT INTO bank.withdrawal VALUES (2, 1, 10.00, '2022-11-08 08:02:57');
+INSERT INTO bank.withdrawal VALUES (3, 1, 5.00, '2022-11-14 07:55:21');
+INSERT INTO bank.withdrawal VALUES (4, 2, 100.00, '2022-11-15 13:12:07');
+INSERT INTO bank.withdrawal VALUES (5, 2, 80.00, '2022-11-17 19:22:03');
+INSERT INTO bank.withdrawal VALUES (6, 3, 20.00, '2022-11-25 15:35:44');
+-- Password 313131;
+INSERT INTO bank.credentials VALUES (13031934, '$2a$12$HcRqgJFcRl4rZ.GrUb19CuEqxab/8Rha0zXUGhc/dpWobErOy1oXW', 1);
+-- Password 999999;
+INSERT INTO bank.credentials VALUES (20101920, '$2a$12$kaLD1nSl98hd5NP2jZm5FOLhbgIlrN7VW87mxM1T9TNVS9zeZQiT.', 2);
+
+-- -----------------------------------------------------
+-- Create test user for test database
+-- -----------------------------------------------------
+CREATE USER IF NOT EXISTS 'bankadmin'@'localhost' IDENTIFIED BY 'bankadminpassword';
+GRANT ALL ON `bank`.* TO 'bankadmin'@'localhost';
