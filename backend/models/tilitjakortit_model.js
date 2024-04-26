99b7ab53fca80e8104b812cc9806d8a7144664ad
MrReaa <152399786+MrReaa@users.noreply.github.com>
2024-04-26 15:44:22 +0300

    poistettu turhat backend tiedot

--- backend/models/tilitjakortit_model.js
+++
@@ -1,9 +0,0 @@
-const db = require('../mods/database');
-
-const tilitjakortit = {
-    getAccountsAndCards: function(asiakas, callback) {
-      return db.query('CALL tilit_ja_kortit(?)', [asiakas], callback);
-    }
-  };
-  
-  module.exports = tilitjakortit;
